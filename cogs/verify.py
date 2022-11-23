e10cac324eae9c5f1ec5eab7d187e5a69c541521
eek7574 <me@nexy7574.cyou>
2022-11-23 11:54:59 +0000

    fix verify command

--- cogs/verify.py
+++ cogs/verify.py
@@ -21,20 +21,20 @@ class VerifyCog(commands.Cog):
 
         role = discord.utils.find(lambda r: r.name.lower() == "verified", ctx.guild.roles)
         channel = discord.utils.get(ctx.guild.text_channels, name="verify")
-        if role and role < ctx.me.top_role:
-            await ctx.author.remove_roles(role, reason=f"Auto de-verified")
-            if channel:
-                try:
-                    await ctx.author.send(
-                        f"You have been automatically de-verified. Please re-verify by going to {channel.mention} and"
-                        f" typing </verify:{ctx.command.id}>."
-                    )
-                except discord.Forbidden:
-                    pass
-            return
-        else:
-            view = VerifyView(ctx)
-            return await ctx.respond(view=view, ephemeral=True)
+        if role in ctx.author.roles:
+            if role and role < ctx.me.top_role:
+                await ctx.author.remove_roles(role, reason=f"Auto de-verified")
+                if channel:
+                    try:
+                        await ctx.author.send(
+                            f"You have been automatically de-verified. Please re-verify by going to {channel.mention} and"
+                            f" typing </verify:{ctx.command.id}>."
+                        )
+                    except discord.Forbidden:
+                        pass
+                return
+        view = VerifyView(ctx)
+        return await ctx.respond(view=view, ephemeral=True)
 
     @commands.command(name="de-verify")
     @commands.is_owner()
