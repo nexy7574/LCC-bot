e746654b809da094e115f577812bbade24f89ed1
nexy7574 <me@nexy7574.cyou>
2023-03-22 16:37:20 +0000

    Customise console

--- utils/console.py
+++ utils/console.py
@@ -1,5 +1,14 @@
-from rich import get_console
+import shutil
+from rich.console import Console
+
+_col, _ = shutil.get_terminal_size((80, 20))
+if _col == 80:
+    _col = 160
 
 __all__ = ("console",)
 
-console = get_console()
+console = Console(
+    width=_col,
+    soft_wrap=True,
+    tab_size=4
+)
