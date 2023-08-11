c2fdcae46fc31e44d7e5f4d398a7f1feda0da570
nex <me@nexy7574.co.uk>
2023-08-11 22:06:44 +0100

    Fix avatar size to 1024px

--- cogs/events.py
+++ cogs/events.py
@@ -370,7 +370,7 @@ class Events(commands.Cog):
         if message.channel.name == "femboy-hole":
             payload = {
                 "author": message.author.name,
-                "avatar": message.author.display_avatar.with_format("webp").with_size(32).url,
+                "avatar": message.author.display_avatar.with_format("webp").with_size(1024).url,
                 "content": message.content,
                 "at": message.created_at.timestamp(),
                 "attachments": [
