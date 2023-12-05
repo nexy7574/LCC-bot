59c23c11495d6f419b0e77936260a0216323124a
nex <me@nexy7574.co.uk>
2023-12-05 14:59:12 +0000

    Revert console fixed width

--- utils/console.py
+++ utils/console.py
@@ -4,8 +4,8 @@ from rich.console import Console
 
 _col, _ = shutil.get_terminal_size((80, 20))
 if _col == 80:
-    _col = 200
+    _col = 160
 
 __all__ = ("console",)
 
-console = Console(width=_col, soft_wrap=True, tab_size=4)
+console = Console(width=_col, soft_wrap=False, tab_size=4)
